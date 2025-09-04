0026a5e118503dc56af7ca2148a85757ae147785
TercihMeselesi <kralcatyt@gmail.com>
2025-09-04 19:35:17 +0300

    Update main

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ import os
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+bot = commands.Bot(command_prefix='!')
+
+kredi = 0
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} olarak giriş yapıldı!')
+    print('------')
+
+@bot.command(name='kredi')
+async def bot_kredi_goster(ctx):
+    await ctx.send(f"Mevcut kredi: **{kredi}**")
+
+bot.run(TOKEN)
